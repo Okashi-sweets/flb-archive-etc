adaf69deb51ae9d26f8b22990e3f500e768416a3
Okashi <soraaltiA@Gmail.com>
2026-04-03 15:05:33 +0900

    update logic

--- ts_component/calculate_rate.ts
+++ ts_component/calculate_rate.ts
@@ -1,26 +1,45 @@
 /*
-このファイルは将来的に使用するレースレートの計算用です。
-まだコードは書きません^^
-すまんやっぱ書くわ
+値の設定
+op_border:opが加算される最低ラインかつレートが最大値になるライン
+rate_border:レートが加算される最低ライン
+base_rate:rate_borderを下回った時のスコア　レート用
+multiplier:op_borderを上回った時のスコア　レート用
+op_score:top_timeと同じ時間の時のスコア　op用(op_borderを下回った時は0なのでbaseはなし)
 */
-export function calc_Rate(
+export async function calc_rate(
     base_rate: number,
     multiplier: number,
     time: number,
     top_time: number,
-    max_percent: number,
-    min_percent: number
+    op_border: number,
+    rate_border: number
 ){
-    if(time / top_time < 1 + min_percent / 100){
-        if(time / top_time < 1 + max_percent / 100){
+    if(time / top_time < 1 + rate_border / 100){
+        if(time / top_time < 1 + op_border / 100){
             return base_rate + multiplier
         }
         const diff = (time / top_time) * 100 - 100
-        const percent = (diff - max_percent) / (min_percent - max_percent)
+        const percent = (rate_border - diff) / (rate_border - op_border)
         const weight = percent ** 2
         return base_rate + multiplier * (1 - weight)
     }
     else{
         return base_rate
+    }
+}
+
+export async function calc_op(
+    time: number,
+    top_time: number,
+    op_border: number,
+    op_score: number
+) {
+    if(time / top_time < 1 + op_border / 100){
+        const diff = (time / top_time) * 100 - 100
+        const percent = (op_border - diff) / op_border
+        return op_score * percent
+    }
+    else{
+        return 0
     }
 }
